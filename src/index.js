c173ea1371a311352609b435593ad8b8ad3d1d28
seongwoon hong <saytoyou91@gmail.com>
2018-04-15 13:22:58 -0400

    Add: redux and router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import reduxThunk from 'redux-thunk';
+
+import reducers from './reducers';
 import './index.css';
 import App from './containers/App/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(reduxThunk)
+);
+
+ReactDOM.render(
+  <Provider store={ store }>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={ App } />
+      </Switch>
+    </BrowserRouter>
+  </Provider>, document.getElementById('root')
+);
