19007f53d2f70c25ff013438eb7dbfc3d957ea5d
seongwoon hong <saytoyou91@gmail.com>
2018-04-15 13:10:31 -0400

    Add: styled-components dependency

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import './App.css';
+import { Segment } from 'semantic-ui-react';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <Segment className="App" basic>
         App
-      </div>
+      </Segment>
     );
   }
 }
