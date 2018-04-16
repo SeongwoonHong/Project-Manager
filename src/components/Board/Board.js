9df8e9bdcca1073910328bfec7c91b27129ac698
seongwoon hong <saytoyou91@gmail.com>
2018-04-16 14:17:29 -0400

    Add: IssueCard component

---
+++ src/components/Board/Board.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import IssueCard from '../IssueCard/IssueCard';
+
+class Board extends Component {
+  render() {
+    return (
+      <div>
+        <IssueCard />
+      </div>
+    );
+  }
+}
+
+export default Board;
+
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
-import { Segment } from 'semantic-ui-react';
+import { Segment, Header, Icon } from 'semantic-ui-react';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import Board from '../../components/Board/Board';
 
 class App extends Component {
   render() {
+    const { match, project } = this.props;
+
     return (
       <Segment className="App" basic>
-        App
+        <Header as="h2">
+          <Icon name="folder open outline" />
+          <Header.Content>
+            { match.params.name }
+          </Header.Content>
+        </Header>
+        <Board project={project} />
       </Segment>
     );
   }
 }
 
-export default App;
+export default connect(state => ({
+  project: state.Project,
+}))(withRouter(App));
+
