f956d8b2548c1daa1700921b2e15be81720d71bc
seongwoon hong <saytoyou91@gmail.com>
2018-05-06 00:21:32 -0400

    Fix: eslint errors in master

--- src/test/routes.js
+++ src/test/routes.js
@@ -6,26 +6,26 @@ const TestRoutes = ({ match }) => (
 
   <Switch>
     {
-       Object.keys(manifestComponents).map((component, i) => {
-         const componentData = manifestComponents[component];
-         let Component;
-         if (i === 0) {
-           Component = componentData.components.default || componentData.component.default;
-         } else {
-           Component = componentData.default || componentData.components.default;
-         }
-         const { props } = componentData;
-         return (
-           <Route
-             path={`${match.url}/${component}`}
-             key={component}
-             render={() =>
-               <Component {...props} />
-             }
-           />
-         );
-       })
-     }
+      Object.keys(manifestComponents).map((component, i) => {
+        const componentData = manifestComponents[component];
+        let Component;
+        if (i === 0) {
+          Component = componentData.components.default || componentData.component.default;
+        } else {
+          Component = componentData.default || componentData.components.default;
+        }
+        const { props } = componentData;
+        return (
+          <Route
+            path={`${match.url}/${component}`}
+            key={component}
+            render={() =>
+              <Component {...props} />
+            }
+          />
+        );
+      })
+    }
   </Switch>
 );
 
